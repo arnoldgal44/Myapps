bc2a91d41083489c05ff8e4383164f2fd4b4a62e
SSZC Szondi György <sszcszondi@hotmail.com>
2019-03-04 13:38:24 +0100

    #1: Fruit osztály létrehozva, konstruktor beállítva

---
+++ MyFirstApp/MyFirstApp/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Fruit
+    {
+
+        public string Nev;
+        public string Szarmhely;
+        public int Mennyiseg;
+
+
+        public Fruit(string aNev,string aSzhely,int aMennyiseg)
+        {
+            Nev = aNev;
+            Szarmhely = aSzhely;
+            Mennyiseg = aMennyiseg;
+
+        }
+
+    }
+}
