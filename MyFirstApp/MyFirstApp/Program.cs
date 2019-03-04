ac8007d9abc7faca60e885f59a3e6d8c7b9a1d51
SSZC Szondi György <sszcszondi@hotmail.com>
2019-03-04 13:46:08 +0100

    #1: Lista készítése (foreach)

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -11,12 +11,21 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
 
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            var fruits = new List<Fruit>();
+
+
+            fruits.Add(new Fruit("Körte","Hungary",50));
+            fruits.Add(new Fruit("Banán","Hungary",50));
+
+
+            foreach (var item in fruits )
+            {
+                Console.WriteLine("Gyümölcs név: " + item.Nev + " hely: " + item.Szarmhely);
+            }
+            
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
     }
 }
